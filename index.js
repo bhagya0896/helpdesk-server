d73b5d03cc1f8a670dcbb4c221b9da2fa6699883
bhagyalakshmi <bhagyalakshmi96.k@gmail.com>
2021-05-23 22:31:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+
+// Routes
+const registerRoute = require('./routes.js');
+const loginRoute = require('./routes.js');
+const activateRoute = require('./routes.js');
+const ticketsRoute= require('./routes.js');
+const addTicketRoute = require('./routes.js');
+const deleteTicketRoute = require('./routes.js');
+const addContactFormRoute = require('./routes.js');
+
+
+const port = process.env.PORT|| 5000
+
+//Middleware
+app.use(express.json());
+app.use(cors());
+
+app.use('/user', registerRoute);
+app.use('/user', loginRoute);
+app.use('/user', activateRoute);
+app.use('/user', ticketsRoute);
+app.use('/user', addTicketRoute);
+app.use('/user', deleteTicketRoute);
+app.use('/user', addContactFormRoute);
+
+
+// setting up of port
+
+    app.listen(port,()=>
+    {
+        console.log(`Connection is established and app started running on port : ${port}!!!`)
+  
+    })
+
+
+
