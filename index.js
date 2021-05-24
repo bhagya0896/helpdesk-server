7e414478db79825c84fa4b3a3d91984cc091a80e
bhagya0896 <77100278+bhagya0896@users.noreply.github.com>
2021-05-24 08:13:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,14 @@ const port = process.env.PORT|| 5000
 //Middleware
 app.use(express.json());
 app.use(cors());
-
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
+    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+});
 app.use('/user', getRoute);
 app.use('/user', registerRoute);
 app.use('/user', loginRoute);
